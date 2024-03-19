ce06da5c5903352f672be0c774831406b5fa3b4d
yelhanfrendi <101817256+yelhanfrendi@users.noreply.github.com>
2024-03-19 15:59:21 +0100

    implémentations des interfaces (test)

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -22,26 +22,39 @@ public class IPokedexTest {
 
     @Before
     public void init() throws PokedexException {
-        pokedex = mock(IPokedex.class);
-        listePokemon = new ArrayList<>();
-        Pokemon pokemon1 = new Pokemon(133, "Aquali", 186, 168, 260, 0, 0, 0, 0, 0);
-        Pokemon pokemon2 = new Pokemon(1, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 0.56);
+        PokemonMetadataProvider metadataProvider = new PokemonMetadataProvider();
+       PokemonFactory pokemonFactory = new PokemonFactory();
+        pokedex = new Pokedex(metadataProvider, pokemonFactory);
+        Pokemon pokemon1 = new Pokemon(
+                0,
+                "Bulbizarre",
+                126,
+                126,
+                90,
+                613,
+                64,
+                4000,
+                4,
+                0.56
+        );
+       Pokemon pokemon2 = new Pokemon(
+                133,
+                "Aquali",
+                186,
+                168,
+                260,
+                2729,
+                202,
+                5000,
+                4,
+                1
+        );
 
-        listePokemon.add(pokemon1);
-        listePokemon.add(pokemon2);
-        doAnswer(invocation -> {
-            Pokemon pokemon = invocation.getArgument(0);
-            listePokemon.add(pokemon);
-            return null;
-        }).when(pokedex).addPokemon(any(Pokemon.class));
-        when(pokedex.size()).thenAnswer(invocation -> listePokemon.size());
-        when(pokedex.getPokemon(133)).thenReturn(pokemon1);
-        when(pokedex.getPokemons()).thenReturn(listePokemon);
-        when(pokedex.getPokemons(any(Comparator.class))).thenAnswer(invocation -> {
-            Comparator<Pokemon> comparator = invocation.getArgument(0);
-            return listePokemon.stream().sorted(comparator).collect(Collectors.toList());
-        });
+        pokedex.addPokemon(pokemon1);
+        pokedex.addPokemon(pokemon2);
 
+
+        PokemonComparators comparator = PokemonComparators.NAME;
 
     }
 
@@ -60,8 +73,8 @@ public class IPokedexTest {
 
     @Test
     public void getPokemon() throws PokedexException {
-        Pokemon result = pokedex.getPokemon(133);
-        assertEquals(133, result.getIndex());
+        Pokemon result = pokedex.getPokemon(0);
+        assertEquals(0, result.getIndex());
     }
 
     @Test
@@ -76,7 +89,7 @@ public class IPokedexTest {
         List<Pokemon> pokemons = pokedex.getPokemons(pokemonComparator);
         assertEquals(2, pokemons.size());
 
-        assertEquals(1, pokemons.get(0).getIndex());
+        assertEquals(0, pokemons.get(0).getIndex());
         assertEquals(133, pokemons.get(1).getIndex());
     }
 }
